8478d2078cc0d4da2338aee95b8bb55b36e44690
Erfan-ffa <109587086+Erfan-ffa@users.noreply.github.com>
2022-07-25 12:58:17 +0430

    Add files via upload

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Queue;
+
+QueueArray x = new(3);
+
+//x.Enqueue(1);
+//x.Enqueue(2);
+//x.Enqueue(3);
+
+//x.PrintMembers();
+
+//Console.WriteLine("-------");
+//Console.WriteLine("poped item :" + x.Dequeue()); // 2 , 3
+//Console.WriteLine("poped item :" + x.Dequeue()); // 2 , 3
+
+
+//x.PrintMembers();
+
+
+//Console.WriteLine("after adding:");
+//x.Enqueue(5);
+//x.PrintMembers();
+
+
+
+
+x.Enqueue(1);
+x.Enqueue(2);
+x.Enqueue(3);
+x.PrintMembers();
+Console.WriteLine(":: " + x.Dequeue());
+x.PrintMembers();
+
+x.Enqueue(5);
+Console.WriteLine(":: " + x.Dequeue());
+x.Enqueue(6);
+x.PrintMembers();
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Queue/Queue/QueueArray.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public class QueueArray
+    {
+        int[] numArray;
+        int arrayLength;
+        int front,rear = 0;
+
+
+
+        public QueueArray(int SizeArray)
+        {
+            arrayLength = SizeArray;
+            numArray = new int[arrayLength];
+        }
+
+        bool IsFull()
+        {
+            if (rear >= arrayLength)
+                return true;
+            return false;
+        }
+
+        bool IsEmpty()
+        {
+            if (rear == front)
+                return true;
+            return false;
+        }
+
+
+        public void Enqueue(int Num)
+        {
+            if (!IsFull())
+            {
+                numArray[rear] = Num;
+                rear++;
+            }
+            else
+            {
+                Console.WriteLine("Queue is full!");
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (!IsEmpty())
+            {
+                int result = numArray[front];
+                for (int i = 0; i < numArray.Length -1 ; i++)
+                {
+                    numArray[i] = numArray[i + 1];
+                }
+                rear--;
+                return result;
+            }
+            return -1;
+        }
+
+        public void PrintMembers()
+        {
+            if (!IsEmpty()) 
+            {
+                for (int i = front; i < rear; i++)
+                {
+                    Console.WriteLine(numArray[i]);
+                }                
+            }
+        }
+    }
+}
